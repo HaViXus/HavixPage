c903c03ed52f0fc22d016fd4ac38e75742482804
HaViXus <tukedarr@gmail.com>
2022-03-28 01:47:52 +0200

    Tiles content layout for HomePage [without images]

---
+++ src/Pages/HomePage/HomePage.styles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const HomePageStyled = styled.div`
+	display: flex;
+	width: 100%;
+	height: 100%;
+	justify-content: center;
+	align-items: center;
+	overflow-y: hidden;
+`;
+
+export const HomePageContent = styled.div`
+	display: flex;
+	position: relative; 
+	width: 90%;
+	height: 89.4%;
+	flex-wrap: wrap;
+	justify-content: center;
+	align-items: center;
+	gap: 12px;
+`;
+
+export const ContentRow = styled.div`
+	display: flex;
+	width: 100%;
+	max-width: 88vw;
+	height: 48.14%;
+	max-height: 22vw;
+	flex-wrap: nowrap;
+	justify-content: center;
+	align-items: center;
+	gap: 12px;
+`;
--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,12 +1,54 @@
 import React from "react";
 import { PageTemplate } from "../PageTemplate/PageTemplate";
-import { useLocation } from "react-router-dom";
+import { ContentRow, HomePageContent, HomePageStyled } from "./HomePage.styles";
+import { MainBoxButton } from "../../Components/MainBoxButton/MainBoxButton";
+import { routerPaths } from "../../Components/Router/RouterPaths";
+import { useNavigate } from "react-router-dom";
+import { RouterKeys } from "../../Components/Router/Router.interfaces";
+
 
 export const HomePage = () => {
-	const location = useLocation();
+	const navigate = useNavigate();
+
+	const contentData: RouterKeys[] = [RouterKeys.GamesMenu, RouterKeys.PixelArts, RouterKeys.SpriteSheetsMenu, RouterKeys.BoxOfStuff];
+
+	const getMainTiles = () => {
+		const MainTiles = contentData.map((key: string, index: number) => {
+			const tileTitle = routerPaths[key].nameToDisplay;
+			const tilePath = routerPaths[key].path; 
+			const onTileClick = () => navigate(tilePath);
+
+			return (
+				<MainBoxButton key={key} title={tileTitle} onClick={onTileClick} />
+			);
+		});
+		return MainTiles;
+	};
+
+	const getMainTilesRows = () => {
+		const MainTiles = getMainTiles();
+		const rowSize = 2;
+		const TilesRows = MainTiles.reduce((previous, tile, index) => {
+			if(index % rowSize === 0) {
+				const nextTile = MainTiles[index+1];
+				const newRow = <ContentRow>{[tile, nextTile]}</ContentRow>;
+				return([...previous, newRow]);
+				
+			} else{
+				return(previous);
+			}
+		}, [] as JSX.Element[]);
+
+		return TilesRows;
+	};
+
 	return(
 		<PageTemplate>
-            aaab
+			<HomePageStyled>
+				<HomePageContent>
+					{getMainTilesRows()}
+				</HomePageContent>
+			</HomePageStyled>
 		</PageTemplate>
 	);
 };
